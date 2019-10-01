8f4b0ed81ca2c9a8c9dc831918e22c06e12edd11
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-01 12:55:31 +0200

    fix for github action context

--- src/uploadCoverage.js
+++ src/uploadCoverage.js
@@ -37,8 +37,41 @@ const readOptionsFromGithubAction = () => {
     build: process.env.GITHUB_WORKFLOW,
     branch: process.env.GITHUB_REF,
     job: process.env.GITHUB_ACTION,
-    pr: "",
+    pr: process.env.GITHUB_EVENT_NAME === "pull_request" ? getPullRequestNumber() : undefined,
     slug: process.env.GITHUB_REPOSITORY,
     root: process.env.GITHUB_WORKSPACE,
   }
 }
+
+// or maybe https://github.community/t5/GitHub-Actions/GitHub-Actions-no-way-to-get-PR-number-on-push-event/td-p/23433
+const getPullRequestNumber = () => {
+  const ref = process.env.GITHUB_REF
+  const pullPrefix = "refs/pull/"
+  const pullRequestNumberStartIndex = ref.indexOf(pullPrefix)
+  if (pullRequestNumberStartIndex === -1) return undefined
+  const afterPull = ref.slice(pullRequestNumberStartIndex + pullPrefix.length)
+  const slashAfterPullIndex = afterPull.indexOf("/")
+  if (slashAfterPullIndex === -1) return undefined
+  const pullRequestNumberString = afterPull.slice(0, slashAfterPullIndex)
+  return Number(pullRequestNumberString)
+}
+
+/**
+ * Example of process.env for a pull request
+ *
+ * {
+ *   process.env.HOME: '/home/runner',
+ *   process.env.GITHUB_WORKFLOW: 'ci',
+ *   process.env.GITHUB_ACTION: 'run',
+ *   process.env.GITHUB_ACTOR: 'dmail',
+ *   process.env.GITHUB_REPOSITORY: 'jsenv/jsenv-codecov-upload',
+ *   process.env.GITHUB_EVENT_NAME: 'pull_request',
+ *   process.env.GITHUB_EVENT_PATH: '/home/runner/work/_temp/_github_workflow/event.json',
+ *   process.env.GITHUB_WORKSPACE: '/home/runner/work/jsenv-codecov-upload/jsenv-codecov-upload',
+ *   process.env.GITHUB_SHA: 'e42f8c0500aa7396f5b4fd893b4fdade8b31f354',
+ *   process.env.GITHUB_REF: 'refs/pull/1/merge',
+ *   process.env.GITHUB_HEAD_REF: 'github-actions',
+ *   process.env.GITHUB_BASE_REF: 'master'
+ * }
+ *
+ */
