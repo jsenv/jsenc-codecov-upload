216fdbffbe3e70797e870a4c300808c04cf7e577
Damien Maillard <damien.maillard@dailymotion.com>
2020-11-06 09:48:15 +0100

    fix common js bundle script

--- dist/commonjs/main.cjs
+++ dist/commonjs/main.cjs
@@ -3,104 +3,11 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 var module$1 = require('module');
-var url$1 = require('url');
-var fs = require('fs');
-require('crypto');
-require('path');
-var util = require('util');
+var util = require('@jsenv/util');
 
-/* global require, __filename */
-const nodeRequire = require;
+/* global __filename */
 const filenameContainsBackSlashes = __filename.indexOf("\\") > -1;
 const url = filenameContainsBackSlashes ? `file:///${__filename.replace(/\\/g, "/")}` : `file://${__filename}`;
-
-const ensureUrlTrailingSlash = url => {
-  return url.endsWith("/") ? url : `${url}/`;
-};
-
-const isFileSystemPath = value => {
-  if (typeof value !== "string") {
-    throw new TypeError(`isFileSystemPath first arg must be a string, got ${value}`);
-  }
-
-  if (value[0] === "/") return true;
-  return startsWithWindowsDriveLetter(value);
-};
-
-const startsWithWindowsDriveLetter = string => {
-  const firstChar = string[0];
-  if (!/[a-zA-Z]/.test(firstChar)) return false;
-  const secondChar = string[1];
-  if (secondChar !== ":") return false;
-  return true;
-};
-
-const fileSystemPathToUrl = value => {
-  if (!isFileSystemPath(value)) {
-    throw new Error(`received an invalid value for fileSystemPath: ${value}`);
-  }
-
-  return String(url$1.pathToFileURL(value));
-};
-
-const assertAndNormalizeDirectoryUrl = value => {
-  let urlString;
-
-  if (value instanceof URL) {
-    urlString = value.href;
-  } else if (typeof value === "string") {
-    if (isFileSystemPath(value)) {
-      urlString = fileSystemPathToUrl(value);
-    } else {
-      try {
-        urlString = String(new URL(value));
-      } catch (e) {
-        throw new TypeError(`directoryUrl must be a valid url, received ${value}`);
-      }
-    }
-  } else {
-    throw new TypeError(`directoryUrl must be a string or an url, received ${value}`);
-  }
-
-  if (!urlString.startsWith("file://")) {
-    throw new Error(`directoryUrl must starts with file://, received ${value}`);
-  }
-
-  return ensureUrlTrailingSlash(urlString);
-};
-
-const urlToFileSystemPath = fileUrl => {
-  if (fileUrl[fileUrl.length - 1] === "/") {
-    // remove trailing / so that nodejs path becomes predictable otherwise it logs
-    // the trailing slash on linux but does not on windows
-    fileUrl = fileUrl.slice(0, -1);
-  }
-
-  const fileSystemPath = url$1.fileURLToPath(fileUrl);
-  return fileSystemPath;
-};
-
-const isWindows = process.platform === "win32";
-
-const resolveUrl = (specifier, baseUrl) => {
-  if (typeof baseUrl === "undefined") {
-    throw new TypeError(`baseUrl missing to resolve ${specifier}`);
-  }
-
-  return String(new URL(specifier, baseUrl));
-};
-
-const isWindows$1 = process.platform === "win32";
-const baseUrlFallback = fileSystemPathToUrl(process.cwd());
-
-const isWindows$2 = process.platform === "win32";
-
-const readFilePromisified = util.promisify(fs.readFile);
-
-const isWindows$3 = process.platform === "win32";
-
-/* eslint-disable import/max-dependencies */
-const isLinux = process.platform === "linux"; // linux does not support recursive option
 
 const require$1 = module$1.createRequire(url);
 
@@ -113,13 +20,13 @@ const uploadCoverage = ({
   token = process.env.CODECOV_TOKEN,
   ...rest
 }) => {
-  projectDirectoryUrl = assertAndNormalizeDirectoryUrl(projectDirectoryUrl);
-  const coverageJsonFileUrl = resolveUrl(coverageJsonFileRelativeUrl, projectDirectoryUrl);
+  projectDirectoryUrl = util.assertAndNormalizeDirectoryUrl(projectDirectoryUrl);
+  const coverageJsonFileUrl = util.resolveUrl(coverageJsonFileRelativeUrl, projectDirectoryUrl);
   const options = { ...rest,
     ...(process.env.GITHUB_REPOSITORY ? readOptionsFromGithubAction() : {}),
     token,
-    file: urlToFileSystemPath(coverageJsonFileUrl),
-    root: urlToFileSystemPath(projectDirectoryUrl)
+    file: util.urlToFileSystemPath(coverageJsonFileUrl),
+    root: util.urlToFileSystemPath(projectDirectoryUrl)
   };
   return new Promise((resolve, reject) => {
     // https://github.com/codecov/codecov-node/blob/023d204c671bc7d66b72261d2da07f2b72da2669/lib/codecov.js#L238
@@ -175,4 +82,5 @@ const getPullRequestNumber = () => {
  */
 
 exports.uploadCoverage = uploadCoverage;
+
 //# sourceMappingURL=main.cjs.map
